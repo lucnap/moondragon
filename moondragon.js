1fabfff37050cd6001fd113e7a46f9735b9b840b
Luciano <lucianonapoli@yahoo.it>
2015-07-19 19:56:57 +0200

    library created

---
+++ moondragon.js
@@ -0,0 +1,24 @@
+
+var Moondragon = (funtion(window) {
+
+	var _publics = {};
+	var _content1;
+	
+	
+	function forgeContent() {
+		return "the content = " + _content1;
+	}
+	
+	
+	_publics.addContent = function(x) {
+		_content1 = _content1 + x;
+	}
+	
+	_publics.getContent = function() {
+	}
+	
+
+	return _publics;
+
+
+})(window);
